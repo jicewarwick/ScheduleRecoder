51e6c76db56295ede1d6afa7b906ebd69604eb71
Ji Ce <jice@yanfenginv.com>
2019-11-19 18:35:46 +0800

    try translation

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <QApplication>
+#include <QTranslator>
 
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    QTranslator translator;
+    translator.load("mainwindow_cn");
+    a.installTranslator(&translator);
     MainWindow w;
     w.show();
     return a.exec();
